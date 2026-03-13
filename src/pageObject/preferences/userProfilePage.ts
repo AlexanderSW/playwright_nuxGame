2a1b936b2bfad57e1f53e1a273076496b66eb586
AlexanderSW <podobulkin@ukr.net>
2026-03-13 10:54:02 +0200

    Edited page file

--- src/pageObject/preferences/userProfilePage.ts
+++ src/pageObject/preferences/userProfilePage.ts
@@ -4,31 +4,30 @@ import { LANGUAGE_OPTIONS_ARRAY } from "../../constants/userPreferences/language
 
 export class UserProfilePage extends BasePage {
 
-    userLinksDropdown: Locator = this.page.locator("#vector-user-links-dropdown-checkbox");
-    userPreferencesLink: Locator = this.page.locator("xpath=//li[@id='pt-preferences']//a[@href='/wiki/Special:Preferences']");
-    userProfileTab: Locator = this.page.locator("xpath=//div[@role='tab'][1]//span");
-    languageSelectField: Locator = this.page.locator("xpath=//select[@id='mw-input-wplanguage']/following::input[1]");
-    savePreferencesButton: Locator = this.page.locator("xpath=//span[@id='prefcontrol']//button");
-    languageTextOnSelect: Locator = this.page.locator("xpath=//select[@id='mw-input-wplanguage']//option[@selected]")
+    private readonly userLinksDropdown: Locator = this.page.locator("#vector-user-links-dropdown-checkbox");
+    private readonly userPreferencesLink: Locator = this.page.locator("xpath=//li[@id='pt-preferences']//a[@href='/wiki/Special:Preferences']");
+    private readonly userProfileTab: Locator = this.page.locator("xpath=//div[@role='tab'][1]//span");
+    private readonly languageSelectField: Locator = this.page.locator("xpath=//select[@id='mw-input-wplanguage']/following::input[1]");
+    private readonly savePreferencesButton: Locator = this.page.locator("xpath=//span[@id='prefcontrol']//button");
+    private readonly languageTextOnSelect: Locator = this.page.locator("xpath=//select[@id='mw-input-wplanguage']//option[@selected]")
 
-    languageOption(languageCode: string, language: string): Locator {
+    private languageOption(languageCode: string, language: string): Locator {
         return this.page.getByRole('option', { name: `${languageCode} - ${language}` });
     }
 
-    getRandomLanguage() {
+    public getRandomLanguage() {
         const index = Math.floor(Math.random() * LANGUAGE_OPTIONS_ARRAY.length);
         return LANGUAGE_OPTIONS_ARRAY[index];
     }
 
-    async navigateToUserProfile() {
+    public async navigateToUserProfile() {
         await this.userLinksDropdown.click();
         await this.userPreferencesLink.click();
-        await this.page.waitForLoadState("networkidle");
+        await expect(this.userProfileTab).toBeVisible();
         await this.userProfileTab.click();
-        await this.page.waitForLoadState("networkidle");
     }
 
-    async chooseLanguage(languageCode: string, language: string) {
+    public async chooseLanguage(languageCode: string, language: string) {
         await this.page.waitForLoadState("domcontentloaded");
         await expect(this.languageSelectField).toBeVisible();
         await this.languageSelectField.clear();
@@ -37,11 +36,11 @@ export class UserProfilePage extends BasePage {
         await this.languageOption(languageCode, language).click();
     }
 
-    async saveUserPreferences() {
+    public async saveUserPreferences() {
         await this.savePreferencesButton.click();
     }
 
-    async checkingInterfaceLanguageChange(languageCode: string, language: string) {
+    public async checkingInterfaceLanguageChange(languageCode: string, language: string) {
         await expect(this.languageTextOnSelect).toBeHidden();
         await expect(this.languageTextOnSelect).toHaveText(`${languageCode} - ${language}`);
     }
